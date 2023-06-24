39bc1c820d6a7ba86ad7a35979ed7076ea312a5d
Chen Ming | Sky <125107275+kohisky@users.noreply.github.com>
2023-06-24 09:10:05 +0000

    rc working without limitation

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 #Main Execution File
 main = True
 while main:
-    option = str(input("Enter the following to proceed.\n1 - create reservation\nExit - End Transaction\n"))
+    option = str(input("Enter the following to proceed.\n1 - create reservation\nExit - End Transaction\nSelect: "))
     if option == "1":
+        print("\nCreating Reservation")
         with open("reservation_create.py") as create:
             exec(create.read())
     if option == "2":
@@ -12,6 +13,3 @@ while main:
     
     if option == "Exit":
         main = False
-    
-
-
--- reservation_create.py
+++ reservation_create.py
@@ -11,38 +11,82 @@ while start:
         check = datetime.date(datetime.now() + timedelta(days=5))
 
         if check <= reservationdate:
-            print(reservationdate)
-            slot = int(input("Please select a slot\n1 - 12:00 pm - 02:00 pm\n2 - 02:00 pm - 04:00 pm\n3 - 06:00 pm - 08:00 pm\n4 - 08:00 pm - 10:00 pm"))
-            if slot == 1:
+            slot = str(input("Please select a slot\n1 for 12:00 pm - 02:00 pm\n2 for 02:00 pm - 04:00 pm\n3 for 06:00 pm - 08:00 pm\n4 for 08:00 pm - 10:00 pm\nSelect: "))
+            if slot == "1":
                 information.append(reservationdate)
-                information.append(slot)
-                name = str(input("Please enter your Name"))
+                information.append("Slot " + str(slot))
+                name = str(input("Please enter your Name: "))
                 information.append(name)
-                email = str(input("Please enter your E-mail address"))
+                email = str(input("Please enter your E-mail address: "))
                 information.append(email)
-                phone_number = str(input("Please enter your phone number"))
+                phone_number = str(input("Please enter your phone number: "))
                 information.append(phone_number)
-                people = int(input("Please enter the number of people (Maximun is 4 person)"))
+                people = int(input("Please enter the number of people (Maximun is 4 person): "))
                 while people >= 5:
-                    people = int(input("Please enter the number of people (Maximun is 4 person)"))
+                    people = int(input("Please enter the number of people (Maximun is 4 person): "))
                 if people <= 4:
                     information.append(people)
-                    with open(r"reservation_StudentID.txt", "w") as info:
-                        info.write("\n".join(str(item) for item in information))
-                        
+                    with open(r"reservation_StudentID.txt", "a") as info:
+                        info.write("|".join(str(item) for item in information) + "\n")
                     start = False
-            if slot == 2:
-                pass
-            if slot == 3:
-                pass
-            if slot == 4:
-                pass
-
-        else: 
+            elif slot == "2":
+                information.append(reservationdate)
+                information.append("Slot " + str(slot))
+                name = str(input("Please enter your Name: "))
+                information.append(name)
+                email = str(input("Please enter your E-mail address: "))
+                information.append(email)
+                phone_number = str(input("Please enter your phone number: "))
+                information.append(phone_number)
+                people = int(input("Please enter the number of people (Maximun is 4 person): "))
+                while people >= 5:
+                    people = int(input("Please enter the number of people (Maximun is 4 person): "))
+                if people <= 4:
+                    information.append(people)
+                    with open(r"reservation_StudentID.txt", "a") as info:
+                        info.write("|".join(str(item) for item in information) + "\n")
+                    start = False
+            elif slot == "3":
+                information.append(reservationdate)
+                information.append("Slot " + str(slot))
+                name = str(input("Please enter your Name: "))
+                information.append(name)
+                email = str(input("Please enter your E-mail address: "))
+                information.append(email)
+                phone_number = str(input("Please enter your phone number: "))
+                information.append(phone_number)
+                people = int(input("Please enter the number of people (Maximun is 4 person): "))
+                while people >= 5:
+                    people = int(input("Please enter the number of people (Maximun is 4 person): "))
+                if people < 5:
+                    information.append(people)
+                    with open(r"reservation_StudentID.txt", "a") as info:
+                        info.write("|".join(str(item) for item in information) + "\n")
+                    print()
+                    start = False
+            elif slot == "4":
+                information.append(reservationdate)
+                information.append("Slot " + str(slot))
+                name = str(input("Please enter your Name: "))
+                information.append(name)
+                email = str(input("Please enter your E-mail address: "))
+                information.append(email)
+                phone_number = str(input("Please enter your phone number: "))
+                information.append(phone_number)
+                people = int(input("Please enter the number of people (Maximun is 4 person): "))
+                while people >= 5:
+                    people = int(input("Please enter the number of people (Maximun is 4 person): "))
+                if people < 5:
+                    information.append(people)
+                    with open(r"reservation_StudentID.txt", "a") as info:
+                        info.write("|".join(str(item) for item in information) + "\n")
+                    print()    
+                    start = False
+            else:
+                print("Invalid Time Slot")
+                start = False
+        else:
             print("Sorry, reservations must be at least 5 days in advance.")
     except:
         print("Error")
-
-
-information = []
 
